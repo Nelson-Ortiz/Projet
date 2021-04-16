a344d27c88539cae450223a25dc3166f4ec881d5
Nelson <nelson.ortizmontalvo@epfl.ch>
2021-04-16 16:38:14 +0200

    added missing lines to threads declarations

--- Project/WallE/move.c
+++ Project/WallE/move.c
@@ -3,7 +3,14 @@
 
 static THD_WORKING_AREA(waMoveDirections, 1024);
 static THD_FUNCTION(MoveDirections, arg) {
+	(void) arg;
+    chRegSetThreadName(__FUNCTION__);
+    while(1){
 
+
+
+    	
+    }
 }
 
 void init_movedirections(void){
--- Project/WallE/obstacle.c
+++ Project/WallE/obstacle.c
@@ -3,7 +3,14 @@
 
 static THD_WORKING_AREA(waObstacleDetection, 1024);
 static THD_FUNCTION(ObstacleDetection, arg) {
+	(void) arg;
+    chRegSetThreadName(__FUNCTION__);
+    while(1){
 
+
+
+    	
+    }
 }
 
 void init_obstacledetection(void){
