e6b16727c893def8c58acad908defdbf2d20ea00
Nelson <nelson.ortizmontalvo@epfl.ch>
2021-04-20 10:06:33 +0200

    motors thread almost finished, sangle2step() needs to be done

--- Project/WallE/motor.c
+++ Project/WallE/motor.c
@@ -2,6 +2,191 @@
 #include "motor.h"
 #include "motors.h"
 
+#define DEFAULT_DIR 0 
+#define SPIRAL_DIR 1 
+#define ANGLE_DIR 2
+
+#define DEFAULT_INS 0 // the robot goes fordward 
+#define SPIRAL_INS 121 
+
+
+
+#define MAX_ANGLE 120 
+#define MIN_ANGLE 1 
+#define LIMIT_RIGHT_QUADRANT 60
+#define LIMIT_LEFT_QUADRANT 120
+
+
+#define NUM_MOTORS 2
+#define RIGHT 0
+#define LEFT 1 
+
+//for the moment the speeds available are defined and constant trough the project
+#define HIGH_SPEED 825 // 75% of the max speed 1100 steps/s
+#define LOW_SPEED 275 // 25% of the max speed 
+
+static uint8_t instruction = 0;
+static int32_t steps2do [NUM_MOTORS];
+static uint8_t direction = DEFAULT_DIR ; 
+
+
+static THD_WORKING_AREA(waMotorControl, 1024);
+static THD_FUNCTION(MotorControl, arg) {
+	(void) arg;
+    chRegSetThreadName(__FUNCTION__);
+    
+
+    while(1){
+
+    	//first we read the instruction that are given
+
+    	if (instruction == DEFAULT_INS )
+    	{
+    		
+    		//go fordward
+
+    		direction = DEFAULT_DIR;
+    	}
+    	
+    	else if (instruction >= MIN_ANGLE && instruction <= MAX_ANGLE)
+    	{
+    		
+    		//we translate the number given into an angle and then into motor steps 
+    		angle2steps(instruction);
+    		
+    		//we check if we are  still turning from a previous iteration or if we already finished 
+    		if (direction==ANGLE_DIR)
+    		{
+    			//if we are already turning from a previous iteration we check the status 
+    			
+    			if (right_motor_get_pos() == 0 && left_motor_get_pos()==0  ) 
+    			{
+    				//if we already finished turning we put the motors in the default status for the next iteration
+    				instruction=DEFAULT_INS;
+    			}
+    			else
+    			{
+    				//if we haven't finished turning we keep the status for cheking on the next itertion
+    				direction=ANGLE_DIR;
+    			}
+    		}
+
+    		else
+    		{
+    			//if we are not turning from a previous iteration we start the turning process 
+    			left_motor_set_pos(steps2do[LEFT]);
+    			right_motor_set_pos(steps2do[RIGHT]);
+
+    			if (instruction<=LIMIT_LEFT_QUADRANT)
+    			{
+    				left_motor_set_speed(LOW_SPEED);
+    				right_motor_set_speed(-LOW_SPEED);
+    			}
+    			else
+    			{
+    				left_motor_set_speed(-LOW_SPEED);
+    				right_motor_set_speed(LOW_SPEED);
+    			}
+    		}
+    	}
+    	else if (instruction=SPIRAL_INS)
+    	{
+    		left_motor_set_speed(LOW_SPEED);
+    		right_motor_set_speed(HIGH_SPEED);
+    		direction=SPIRAL_DIR;
+    	}
+		else
+		{
+			left_motor_set_speed(0);
+    		right_motor_set_speed(0);
+		}
+    	
+    }
+}
+
+
+void set_direction_motors(uint8_t direction2follow){
+	
+	instruction = direction2follow; 
+
+}
+
+void inti_th_motor(void){
+     chThdCreateStatic(waMotorControl, sizeof(waMotorControl), NORMALPRIO, MotorControl, NULL);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 #define DEFAULT 0 // can be spiral or stop
 #define FORWARD 1
 #define BACKWARD 2 
@@ -10,27 +195,6 @@
 #define SPIRAL 5 
 #define STOP 6
 #define NUM_DIRECTION 7 
-
-//for the moment the speeds available are defined and constant trough the project
-#define HIGH_SPEED 825 // 75% of the max speed 1100 steps/s
-#define LOW_SPEED 275 // 25% of the max speed 
-
-
-//static uint8_t direction[NUM_DIRECTION]; 
-
-/*
-static THD_WORKING_AREA(waMotorControl, 1024);
-static THD_FUNCTION(MotorControl, arg) {
-	(void) arg;
-    chRegSetThreadName(__FUNCTION__);
-    while(1){
-
-
-
-    	
-    }
-}*/
-
 void instruction_motor(uint8_t direction){
 	switch(direction) {
 		
@@ -85,7 +249,5 @@ void instruction_motor(uint8_t direction){
 		right_motor_set_speed(0);
 		left_motor_set_speed(0);
 	}
-
-
-
 }
+*/
--- Project/WallE/motor.h
+++ Project/WallE/motor.h
@@ -5,11 +5,35 @@
  /**
  * @brief  Set the robot to go to the direction indicated
  * 
- * @param direction		Default(0); Forward(1); Backward(2);Left(3);
- *						Right(4);Spiral(5); stop(6)
- *						Otherwise it's set to stop
+ * @param direction		0 : default, fordward in this case
+ *						1-120: translated in an anlge for the robot
+ *						121 : spiral
+ *						122-127: others 
  */
+void set_direction_motors(uint8_t direction2follow);
 
-void instruction_motor(uint8_t direction);
+ /**
+ * @brief   initialize the motor control thread and other parameters
+ * 
+ *
+ */
+void inti_th_motor(void);
+
 
 #endif
+
+
+
+
+//template 
+ /**
+ * @brief   Configures some parameters of the camera
+ * 
+ * @param fmt           format of the image. See format_t
+ * @param imgsize       size of the image. See image_size_t
+ *
+ * @return              The operation status.
+ * @retval MSG_OK       if the function succeeded.
+ * @retval MSG_TIMEOUT  if a timeout occurred before operation end or if wrong imgsize
+ *
+ */
